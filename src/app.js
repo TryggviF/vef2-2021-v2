a9d70f0710e957e809d907278b4d5db92b7ec52e
Tryggvi Freyr Sigurgeirsson <57729908+TryggviF@users.noreply.github.com>
2021-02-11 13:40:22 +0000

    First steps

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const {
   PORT: port = 3000,
 } = process.env;
 
 const app = express();
-
+app.set('views', './views');
+app.set('view engine', 'ejs');
 // TODO setja upp rest af virkni!
+app.get('/', async (req, res) => {
+  res.render('form');
+});
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
